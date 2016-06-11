286b6eae439d9b74ac4d2cdf9d8dc8dd502a45ff
Ivan Gulin <doctor254452009@ya.ru>
2016-06-12 00:35:51 +0600

    Создана таблица с записями, начат админ контроллер

--- www/controllers/adminController.php
+++ www/controllers/adminController.php
@@ -1,3 +1,13 @@
 <?php
-	echo "admin panel";
+	require_once("models/article.php");
+	$records = getAllRecords($link);
+	// if (@$_POST['enter']) //$_SERVER['REQUEST_METHOD'] =="POST"
+	// 	addComment($link,$articleNumber,$_POST['created'], $_POST['author'], $_POST['comment']);
+
+	// $record = getRecord($link, $articleNumber);
+	// $comments = getAllComments($link, $articleNumber);
+	if ($Module == "editArticle") include("views/articleEdit.php");
+	else if ($Module == "createArticle" );
+	else if ($Module == "editArticle" );
+	else include("views/admin.php");
 ?>
---
+++ www/views/admin.php
@@ -0,0 +1,33 @@
+<?php 
+head("Панель администратора");
+?>
+<div class="table">
+	<table >
+		<tr>
+			<th>ID</th>
+			<th>Заголовок</th>
+			<th>Контент</th>
+			<th>Дата</th>
+			<th>Редактирование</th>
+			<th>Удаление</th>
+		</tr>
+		<?php
+		foreach ($records as &$record){
+			echo '
+			<tr>
+				<td>'.$record['id'].'</td>
+				<td>'.$record['title'].'</td> 
+				<td>'.substr($record['content'],0,strripos(substr($record['content'],0,700),' ')).'</td>
+				<td>'.$record['date'].'</td>
+				<td><a href="admin/editArticle/id/'.$record['id'].'">Редактировать</a></td>
+				<td><a href="deleteArticle/id/'.$record['id'].'">Удалить</a></td>
+			</tr>';
+		}
+		?>
+	</table>
+	<a href="admin/createArticle">Добавить запись</a>
+</div>
+
+<?php 
+footer();
+?>
